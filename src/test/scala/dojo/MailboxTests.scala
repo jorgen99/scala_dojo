4d575b7c737a4bbef13d480f24ad0c0b34f7d766
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2012-04-03 08:04:05 +0200

    Replaced test() with ignore() in all but the first test case.
    Instead of generating errors the sbt output is not so noisy.
    Only the first test fail and the rest are marked as ignored.

--- src/test/scala/dojo/MailboxTests.scala
+++ src/test/scala/dojo/MailboxTests.scala
@@ -14,38 +14,38 @@ class MailboxTests extends FunSuite with ShouldMatchers{
     response should equal (Pong)
   }
 
-  test("should return ping when sent pong"){
+  ignore("should return ping when sent pong"){
     val response = new MailBox().handleMsg(Pong)
     response should equal (Ping)
   }
 
-  test("should return pong when received ping"){
+  ignore("should return pong when received ping"){
     val response = new MailBox().receive(Ping)
     response should equal (Pong)
   }
 
-  test("should return ping when received pong"){
+  ignore("should return ping when received pong"){
     val response = new MailBox().receive(Pong)
     response should equal (Ping)
   }
 
-  test("should return stopped when received stop message"){
+  ignore("should return stopped when received stop message"){
     val response = new MailBox().receive("stop")
     response should equal ("stopped")
   }
 
-  test("should throw exception when go message received"){
+  ignore("should throw exception when go message received"){
     intercept[MatchError] {
       new MailBox().receive("go")
     }
   }
 
-  test("should return Hello World when received echo message"){
+  ignore("should return Hello World when received echo message"){
     val response = new MailBox().receive(("echo", "Hello World"))
     response should equal ("Hello World")
   }
 
-  test("should return Goodbye World when received echo message with Goodbye World payload"){
+  ignore("should return Goodbye World when received echo message with Goodbye World payload"){
     val response = new MailBox().receive(("echo", "Goodbye World"))
     response should equal ("Goodbye World")
   }
